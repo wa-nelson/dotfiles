b0bd6f6dcc08449910580dfe6bc809073b0d37ec
Alex Nelson <wa.nelson@yahoo.com>
2020-02-27 16:25:40 -0600

    4k i3 configuration

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u 1000 -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar topright &
+polybar topleft &
+
+echo Polybar launched...
