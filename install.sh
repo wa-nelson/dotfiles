20cce6d4fa274b879ea77f8868bc79b5fca6a8df
Alex Nelson <wa.nelson@yahoo.com>
2020-10-26 14:26:49 -0500

    Added i3-gaps install script

--- install.sh
+++ install.sh
@@ -17,8 +17,6 @@ yes() {
 prompt "Apply symbolic links? "
 
 if yes "$response"; then
-		########## Variables
-
 		dir=~/.local/dotfiles                    # dotfiles directory
 
 		# list of files/folders to symlink in homedir
@@ -70,7 +68,22 @@ prompt "Install i3-gaps and polybar? "
 if yes "$response"; then
 		packages+="libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev xutils-dev libxcb-shape0-dev autoconf cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2"
 
+		# Install packages
 		sudo apt install $packages &&
+
+		# i3-gaps on debian
+		cd /tmp &&
+		git clone https://www.github.com/Airblader/i3 i3-gaps &&
+		cd i3-gaps &&
+		autoreconf --force --install &&
+		mkdir build &&
+		cd build &&
+		../configure --prefix=/usr --sysconfdir=/etc &&
+		make &&
+		sudo make install &&
+
+
+		# polybar on debian
 		git clone https://github.com/jaagr/polybar.git &&
 		cd polybar &&
 		git checkout 3.4.1 &&
